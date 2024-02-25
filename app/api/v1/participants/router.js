f24df3eb5a58f214cbb43926a883ac2bfb35bd46
Mohammad Ramdan Ahmad Ramzi <ramdanahamd@gmail.com>
2024-02-25 17:19:47 +0700

    All done fix

--- app/api/v1/participants/router.js
+++ app/api/v1/participants/router.js
@@ -7,8 +7,8 @@ const {
   getAllLandingPage,
   getDetailLandingPage,
   getDashboard,
-  checkout
-//   getAllPayment
+  checkout,
+  getAllPayment
 } = require('./controller')
 
 const { authenticateParticipant } = require('../../../middlewares/auth')
@@ -18,7 +18,7 @@ router.post('/auth/signin', signin)
 router.put('/active', activeParticipant)
 router.get('/events', getAllLandingPage)
 router.get('/events/:id', getDetailLandingPage)
-// router.get('/payments/:organizer', authenticateParticipant, getAllPayment)
+router.get('/payments/:organizer', authenticateParticipant, getAllPayment)
 router.get('/orders', authenticateParticipant, getDashboard)
 router.post('/checkout', authenticateParticipant, checkout)
 
